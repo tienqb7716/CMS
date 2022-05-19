2eaa6bd2dc1fad3da76cc67cd8e4ec2dea78d7f0
Hang Thanh Tuan <tuanht.02.student@fit.tdc.edu.vn>
2022-05-19 15:18:40 +0700

    Footer complete

--- wp-content/themes/group-6/footer.php
+++ wp-content/themes/group-6/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -13,35 +14,35 @@
 
 <footer id="colophon" class="site-footer">
 
-<div class="bg-primary text-white pt-5 pb-5">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-6 col-md-2">
-				<?php dynamic_sidebar( 'footer-widget-col-one' ); ?>
-			</div>
+	<div class="text-white pt-5 pb-5 bg-green">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-md-3">
+					<?php dynamic_sidebar('footer-widget-col-one'); ?>
+				</div>
 
-			<div class="col-sm-6 col-md-2">
-				<?php dynamic_sidebar( 'footer-widget-col-two' ); ?>
-			</div>
+				<div class="col-sm-6 col-md-3">
+					<?php dynamic_sidebar('footer-widget-col-two'); ?>
+				</div>
 
-			<div class="col-md-4 col-sm-12 ms-auto">
-				<?php dynamic_sidebar( 'footer-widget-col-three' ); ?>
+				<div class="col-md-6 col-sm-12 ms-auto">
+					<?php dynamic_sidebar('footer-widget-col-three'); ?>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
 
-<div class="container pt-2 pb-2">
-	<div class="row d-flex align-items-center">
-		<div class="col">
-			<p>&copy; <?php bloginfo( 'name' );?> <?php echo date('Y'); ?> / Created by <a href="https://tuanht99.github.io" target="_blank">Group 6 Shop</a>
-		</div>
-		<div class="col h-25 d-inline-block text-end">
-			<img src="<?php echo get_template_directory_uri(); ?>/img/payment-methods.png" class="img-fluid" loading="lazy" alt="...">
+	<div class="container pt-2 pb-2">
+		<div class="row d-flex align-items-center">
+			<div class="col">
+				<p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?> / Created by <a href="https://tuanht99.github.io" target="_blank">Group 6 Shop</a>
+			</div>
+			<div class="col h-25 d-inline-block text-end">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/payment-methods.png" class="img-fluid" loading="lazy" alt="...">
+			</div>
 		</div>
 	</div>
-</div>
 
 
 
@@ -51,4 +52,5 @@
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
