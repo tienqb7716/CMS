e7e037548000d8e02bfa5025b8660ed9349556ec
LinhTrang <20211tt0755@mail.tdc.edu.vn>
2022-05-27 09:32:28 +0700

    Merge pull request #3 from tientvtdc/Tien
    Complete website

--- wp-content/themes/twentynineteen/page.php
+++ wp-content/themes/twentynineteen/page.php
@@ -24,9 +24,9 @@ get_header();
 				get_template_part( 'template-parts/content/content', 'page' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+				// if ( comments_open() || get_comments_number() ) {
+				// 	comments_template();
+				// }
 
 			endwhile; // End the loop.
 			?>
